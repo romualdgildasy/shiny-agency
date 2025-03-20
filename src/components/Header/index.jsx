68445b9cc95e2ca4d4690246f5a07cec7e2eb1fc
Romuald <romualdgildas6@gmail.com>
2025-03-20 15:18:19 +0100

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import { Outlet, Link } from 'react-router'
+
+function Survey() {
+  return (
+    <div>
+      <h1>Questionnaire 🧮</h1>
+      <Link to="client">Questionnaire Client</Link>
+      <Link to="freelance">Questionnaire Freelance</Link>
+      <Outlet />
+    </div>
+  )
+}
+export default Survey
---
+++ src/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import ClientForm from './components/ClientForm'
+// import Results from './pages/Results'
+// import Freelances from './pages/Freelances'
+import Header from './components/Header'
+// import Error from './components/Error'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+    
+        <Route path="/client" element={<ClientForm />} />
+        {/* <Route path="/results" element={<Results />} />
+        <Route path="/freelances" element={<Freelances />} />
+        <Route path="*" element={<Error />} /> */}
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
