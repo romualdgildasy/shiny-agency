e0552a55f3f39a22dd26b6e681b62d108a2361ee
Romuald <romualdgilads6@gmail.com>
2025-04-10 20:47:43 +0100

    installation des dépendances, les proptypes,les styled

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,34 @@
-import { Outlet, Link } from 'react-router'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { StyledLink } from '../../utils/style/Atoms'
+import DarkLogo from '../../assets/dark-logo.png'
 
-function Survey() {
+const HomeLogo = styled.img`
+  height: 70px;
+`
+
+const NavContainer = styled.nav`
+  padding: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+function Header() {
   return (
-    <div>
-      <h1>Questionnaire 🧮</h1>
-      <Link to="client">Questionnaire Client</Link>
-      <Link to="freelance">Questionnaire Freelance</Link>
-      <Outlet />
-    </div>
+    <NavContainer>
+      <Link to="/">
+        <HomeLogo src={DarkLogo} />
+      </Link>
+      <div>
+        <StyledLink to="/">Accueil</StyledLink>
+        <StyledLink to="/freelances">Profils</StyledLink>
+        <StyledLink to="/survey/1" $isFullLink>
+          Faire le test
+        </StyledLink>
+      </div>
+    </NavContainer>
   )
 }
-export default Survey
+
+export default Header
--- src/index.jsx
+++ src/index.jsx
@@ -3,24 +3,33 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
-import ClientForm from './components/ClientForm'
-// import Results from './pages/Results'
-// import Freelances from './pages/Freelances'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
 import Header from './components/Header'
-// import Error from './components/Error'
+import Error from './components/Error'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+
+    body {
+      margin: 0;
+    }
+`
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey/:questionNumber" element={<Survey />} />
-    
-        <Route path="/client" element={<ClientForm />} />
-        {/* <Route path="/results" element={<Results />} />
+        <Route path="/results" element={<Results />} />
         <Route path="/freelances" element={<Freelances />} />
-        <Route path="*" element={<Error />} /> */}
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   </React.StrictMode>,
